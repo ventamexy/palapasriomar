5fae90d5c8d90e79f54a676caf02c68987e2faec
TI Desarrollo Software <venta.mexy@gmail.com>
2022-07-15 11:49:36 -0500

    Se agregó Trabajos realizados.

--- js/custom.js
+++ js/custom.js
@@ -61,48 +61,6 @@
   });
 
   /* ..............................................
-	   Special Menu
-	   ................................................. */
-
-  var Container = $(".container");
-  Container.imagesLoaded(function () {
-    var portfolio = $(".special-menu");
-    portfolio.on("click", "button", function () {
-      $(this).addClass("active").siblings().removeClass("active");
-      var filterValue = $(this).attr("data-filter");
-      $grid.isotope({
-        filter: filterValue,
-      });
-    });
-    var $grid = $(".special-list").isotope({
-      itemSelector: ".special-grid",
-    });
-  });
-
-  /* ..............................................
-	   BaguetteBox
-	   ................................................. */
-
-  baguetteBox.run(".tz-gallery", {
-    animation: "fadeIn",
-    noScrollbars: true,
-  });
-
-  /* ..............................................
-	   Offer Box
-	   ................................................. */
-
-  $(".offer-box").inewsticker({
-    speed: 3000,
-    effect: "fade",
-    dir: "ltr",
-    font_size: 13,
-    color: "#ffffff",
-    font_family: "Montserrat, sans-serif",
-    delay_after: 1000,
-  });
-
-  /* ..............................................
 	   Tooltip
 	   ................................................. */
 
